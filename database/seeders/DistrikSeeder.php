c13c34f28dd4f13330a1dac00c59693b59c790b8
Samuel Bosawer <zam.bosawer@gmail.com>
2024-12-19 15:02:10 +0900

    Update Seeder Kelurahan

--- database/seeders/DistrikSeeder.php
+++ database/seeders/DistrikSeeder.php
@@ -15,7 +15,7 @@ class DistrikSeeder extends Seeder
     {
         $disrik = Distrik::create([
             'nama_distrik' => 'KAUREH',
-            'latitude' => '3.12739',
+            'latitude' => '-3.12739',
             'longitude' =>  '139.9425241',
             'keterangan' =>  ''
         ]);
@@ -24,132 +24,132 @@ class DistrikSeeder extends Seeder
 
         $disrik = Distrik::create([
             'nama_distrik' => 'AIRU',
-            'latitude' => '',
-            'longitude' =>  '',
+            'latitude' => '-3.5359541',
+            'longitude' =>  '139.7382349',
             'keterangan' =>  ''
         ]);
 
 
         $disrik = Distrik::create([
             'nama_distrik' => 'YAPSI',
-            'latitude' => '',
-            'longitude' =>  '',
+            'latitude' => '-2.8542088',
+            'longitude' =>  '139.968122',
             'keterangan' =>  ''
         ]);
 
         $disrik = Distrik::create([
             'nama_distrik' => 'KEMTUK',
-            'latitude' => '',
-            'longitude' =>  '',
+            'latitude' => '-2.6629485',
+            'longitude' =>  '140.2720839',
             'keterangan' =>  ''
         ]);
 
 
         $disrik = Distrik::create([
             'nama_distrik' => 'KEMTUK GRESI',
-            'latitude' => '',
-            'longitude' =>  '',
+            'latitude' => '-2.7318597',
+            'longitude' =>  '140.1382353',
             'keterangan' =>  ''
         ]);
 
         $disrik = Distrik::create([
             'nama_distrik' => 'GRESI SELATAN',
-            'latitude' => '',
-            'longitude' =>  '',
+            'latitude' => '-2.7064397',
+            'longitude' =>  '140.108867',
             'keterangan' =>  ''
         ]);
 
 
         $disrik = Distrik::create([
             'nama_distrik' => 'NIMBORAN',
-            'latitude' => '',
-            'longitude' =>  '',
+            'latitude' => '-2.6344598',
+            'longitude' =>  '140.0591511',
             'keterangan' =>  ''
         ]);
 
         $disrik = Distrik::create([
             'nama_distrik' => 'NAMBLONG',
-            'latitude' => '',
-            'longitude' =>  '',
+            'latitude' => '-2.6049849',
+            'longitude' =>  '140.1946339',
             'keterangan' =>  ''
         ]);
 
         $disrik = Distrik::create([
             'nama_distrik' => 'NIMBOKRANG',
-            'latitude' => '',
-            'longitude' =>  '',
+            'latitude' => '-2.520923',
+            'longitude' =>  '140.0867401',
             'keterangan' =>  ''
         ]);
 
         $disrik = Distrik::create([
             'nama_distrik' => 'UNURUM GUAY',
-            'latitude' => '',
-            'longitude' =>  '',
+            'latitude' => '-2.6599739',
+            'longitude' =>  '139.4441592',
             'keterangan' =>  ''
         ]);
 
         $disrik = Distrik::create([
             'nama_distrik' => 'DEMTA',
-            'latitude' => '',
-            'longitude' =>  '',
+            'latitude' => '-2.3884571',
+            'longitude' =>  '140.004733',
             'keterangan' =>  ''
         ]);
 
         $disrik = Distrik::create([
             'nama_distrik' => 'YOKARI',
-            'latitude' => '',
-            'longitude' =>  '',
+            'latitude' => '-2.4667549',
+            'longitude' =>  '140.1585271',
             'keterangan' =>  ''
         ]);
 
         $disrik = Distrik::create([
             'nama_distrik' => 'DEPAPRE',
-            'latitude' => '',
-            'longitude' =>  '',
+            'latitude' => '-2.4512246',
+            'longitude' =>  '140.3395435',
             'keterangan' =>  ''
         ]);
 
         $disrik = Distrik::create([
             'nama_distrik' => 'RAVENIRARA',
-            'latitude' => '',
-            'longitude' =>  '',
+            'latitude' => '-2.470554',
+            'longitude' =>  '140.4602819',
             'keterangan' =>  ''
         ]);
 
         $disrik = Distrik::create([
             'nama_distrik' => 'SENTANI BARAT',
-            'latitude' => '',
-            'longitude' =>  '',
+            'latitude' => '-2.5281694',
+            'longitude' =>  '140.2812836',
             'keterangan' =>  ''
         ]);
 
 
         $disrik = Distrik::create([
             'nama_distrik' => 'WAIBHU',
-            'latitude' => '',
-            'longitude' =>  '',
+            'latitude' => '-2.5663889',
+            'longitude' =>  '140.3545814',
             'keterangan' =>  ''
         ]);
 
         $disrik = Distrik::create([
             'nama_distrik' => 'SENTANI',
-            'latitude' => '',
-            'longitude' =>  '',
+            'latitude' => '-2.5675868',
+            'longitude' =>  '140.4357905',
             'keterangan' =>  ''
         ]);
 
         $disrik = Distrik::create([
             'nama_distrik' => 'EMBUNGFAUW',
-            'latitude' => '',
-            'longitude' =>  '',
+            'latitude' => '-2.6570516',
+            'longitude' =>  '140.469072',
             'keterangan' =>  ''
         ]);
 
 
         $disrik = Distrik::create([
             'nama_distrik' => 'SENTANI TIMUR',
-            'latitude' => '',
-            'longitude' =>  '',
+            'latitude' => '-2.6160503',
+            'longitude' =>  '140.5019486',
             'keterangan' =>  ''
         ]);
     }
--- database/seeders/KelurahanSeeder.php
+++ database/seeders/KelurahanSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Kelurahan;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,272 @@ class KelurahanSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+    // Distrik Kaureh
+        $kelurahan = Kelurahan::create([
+            'nama_kelurahan' => 'SOSKOTEK',
+            'latitude' => '',
+            'longitude' =>  '',
+            'distrik_id' =>  '1',
+            'keterangan' =>  ''
+        ]);
+        $kelurahan = Kelurahan::create([
+            'nama_kelurahan' => 'LAPUA',
+            'latitude' => '',
+            'longitude' =>  '',
+            'distrik_id' =>  '1',
+            'keterangan' =>  ''
+        ]);
+        $kelurahan = Kelurahan::create([
+            'nama_kelurahan' => 'SEBUM',
+            'latitude' => '',
+            'longitude' =>  '',
+            'distrik_id' =>  '1',
+            'keterangan' =>  ''
+        ]);
+        $kelurahan = Kelurahan::create([
+            'nama_kelurahan' => 'UMBRON',
+            'latitude' => '',
+            'longitude' =>  '',
+            'distrik_id' =>  '1',
+            'keterangan' =>  ''
+        ]);
+        $kelurahan = Kelurahan::create([
+            'nama_kelurahan' => 'YADAUW',
+            'latitude' => '',
+            'longitude' =>  '',
+            'distrik_id' =>  '1',
+            'keterangan' =>  ''
+        ]);
+
+
+    // Distrik Kaureh
+        $kelurahan = Kelurahan::create([
+            'nama_kelurahan' => 'HULU ATAS',
+            'latitude' => '',
+            'longitude' =>  '',
+            'distrik_id' =>  '2',
+            'keterangan' =>  ''
+        ]);
+        $kelurahan = Kelurahan::create([
+            'nama_kelurahan' => 'AURINA',
+            'latitude' => '',
+            'longitude' =>  '',
+            'distrik_id' =>  '2',
+            'keterangan' =>  ''
+        ]);
+        $kelurahan = Kelurahan::create([
+            'nama_kelurahan' => 'MUARA NAWA',
+            'latitude' => '',
+            'longitude' =>  '',
+            'distrik_id' =>  '2',
+            'keterangan' =>  ''
+        ]);
+        $kelurahan = Kelurahan::create([
+            'nama_kelurahan' => 'PAGAI',
+            'latitude' => '',
+            'longitude' =>  '',
+            'distrik_id' =>  '2',
+            'keterangan' =>  ''
+        ]);
+        $kelurahan = Kelurahan::create([
+            'nama_kelurahan' => 'KAMIKARU',
+            'latitude' => '',
+            'longitude' =>  '',
+            'distrik_id' =>  '2',
+            'keterangan' =>  ''
+        ]);
+        $kelurahan = Kelurahan::create([
+            'nama_kelurahan' => 'NAIRA',
+            'latitude' => '',
+            'longitude' =>  '',
+            'distrik_id' =>  '2',
+            'keterangan' =>  ''
+        ]);
+
+
+    // Distrik Kaureh
+        $kelurahan = Kelurahan::create([
+            'nama_kelurahan' => 'TABBEYAN',
+            'latitude' => '',
+            'longitude' =>  '',
+            'distrik_id' =>  '3',
+            'keterangan' =>  ''
+        ]);
+        $kelurahan = Kelurahan::create([
+            'nama_kelurahan' => 'ONGAN JAYA',
+            'latitude' => '',
+            'longitude' =>  '',
+            'distrik_id' =>  '3',
+            'keterangan' =>  ''
+        ]);
+        $kelurahan = Kelurahan::create([
+            'nama_kelurahan' => 'KWARJA',
+            'latitude' => '',
+            'longitude' =>  '',
+            'distrik_id' =>  '3',
+            'keterangan' =>  ''
+        ]);
+        $kelurahan = Kelurahan::create([
+            'nama_kelurahan' => 'BUMI SAHAJA',
+            'latitude' => '',
+            'longitude' =>  '',
+            'distrik_id' =>  '3',
+            'keterangan' =>  ''
+        ]);
+        $kelurahan = Kelurahan::create([
+            'nama_kelurahan' => 'NAWA MULYA',
+            'latitude' => '',
+            'longitude' =>  '',
+            'distrik_id' =>  '3',
+            'keterangan' =>  ''
+        ]);
+        $kelurahan = Kelurahan::create([
+            'nama_kelurahan' => 'NAWA MUKTI',
+            'latitude' => '',
+            'longitude' =>  '',
+            'distrik_id' =>  '3',
+            'keterangan' =>  ''
+        ]);
+        $kelurahan = Kelurahan::create([
+            'nama_kelurahan' => 'TAKWA BANGUN',
+            'latitude' => '',
+            'longitude' =>  '',
+            'distrik_id' =>  '3',
+            'keterangan' =>  ''
+        ]);
+        $kelurahan = Kelurahan::create([
+            'nama_kelurahan' => 'PURNAWA JATI',
+            'latitude' => '',
+            'longitude' =>  '',
+            'distrik_id' =>  '3',
+            'keterangan' =>  ''
+        ]);
+        $kelurahan = Kelurahan::create([
+            'nama_kelurahan' => 'BUNDRU',
+            'latitude' => '',
+            'longitude' =>  '',
+            'distrik_id' =>  '3',
+            'keterangan' =>  ''
+        ]);
+
+
+    // Kemtuk
+        $kelurahan = Kelurahan::create([
+            'nama_kelurahan' => 'KWANSU',
+            'latitude' => '',
+            'longitude' =>  '',
+            'distrik_id' =>  '3',
+            'keterangan' =>  ''
+        ]);
+        $kelurahan = Kelurahan::create([
+            'nama_kelurahan' => 'NAMBON',
+            'latitude' => '',
+            'longitude' =>  '',
+            'distrik_id' =>  '3',
+            'keterangan' =>  ''
+        ]);
+        $kelurahan = Kelurahan::create([
+            'nama_kelurahan' => 'NAMEI',
+            'latitude' => '',
+            'longitude' =>  '',
+            'distrik_id' =>  '3',
+            'keterangan' =>  ''
+        ]);
+        $kelurahan = Kelurahan::create([
+            'nama_kelurahan' => 'MAMDA',
+            'latitude' => '',
+            'longitude' =>  '',
+            'distrik_id' =>  '3',
+            'keterangan' =>  ''
+        ]);
+        $kelurahan = Kelurahan::create([
+            'nama_kelurahan' => 'MANDAYAWAN',
+            'latitude' => '',
+            'longitude' =>  '',
+            'distrik_id' =>  '3',
+            'keterangan' =>  ''
+        ]);
+        $kelurahan = Kelurahan::create([
+            'nama_kelurahan' => 'SAMA',
+            'latitude' => '',
+            'longitude' =>  '',
+            'distrik_id' =>  '3',
+            'keterangan' =>  ''
+        ]);
+        $kelurahan = Kelurahan::create([
+            'nama_kelurahan' => 'SOAIB',
+            'latitude' => '',
+            'longitude' =>  '',
+            'distrik_id' =>  '3',
+            'keterangan' =>  ''
+        ]);
+        $kelurahan = Kelurahan::create([
+            'nama_kelurahan' => 'SABEAB KECIL',
+            'latitude' => '',
+            'longitude' =>  '',
+            'distrik_id' =>  '3',
+            'keterangan' =>  ''
+        ]);
+        $kelurahan = Kelurahan::create([
+            'nama_kelurahan' => 'SEKORI',
+            'latitude' => '',
+            'longitude' =>  '',
+            'distrik_id' =>  '3',
+            'keterangan' =>  ''
+        ]);
+        $kelurahan = Kelurahan::create([
+            'nama_kelurahan' => 'SKOAIM',
+            'latitude' => '',
+            'longitude' =>  '',
+            'distrik_id' =>  '3',
+            'keterangan' =>  ''
+        ]);
+        $kelurahan = Kelurahan::create([
+            'nama_kelurahan' => 'AIB',
+            'latitude' => '',
+            'longitude' =>  '',
+            'distrik_id' =>  '3',
+            'keterangan' =>  ''
+        ]);
+        $kelurahan = Kelurahan::create([
+            'nama_kelurahan' => 'BENGGUIN PROGO',
+            'latitude' => '',
+            'longitude' =>  '',
+            'distrik_id' =>  '3',
+            'keterangan' =>  ''
+        ]);
+        $kelurahan = Kelurahan::create([
+            'nama_kelurahan' => 'AIMBE',
+            'latitude' => '',
+            'longitude' =>  '',
+            'distrik_id' =>  '3',
+            'keterangan' =>  ''
+        ]);
+
+
+    // Kemtuk Gresi
+    $kelurahan = Kelurahan::create([
+        'nama_kelurahan' => 'AIMBE',
+        'latitude' => '',
+        'longitude' =>  '',
+        'distrik_id' =>  '3',
+        'keterangan' =>  ''
+    ]);
+    $kelurahan = Kelurahan::create([
+        'nama_kelurahan' => 'AIMBE',
+        'latitude' => '',
+        'longitude' =>  '',
+        'distrik_id' =>  '3',
+        'keterangan' =>  ''
+    ]);
+    $kelurahan = Kelurahan::create([
+        'nama_kelurahan' => 'AIMBE',
+        'latitude' => '',
+        'longitude' =>  '',
+        'distrik_id' =>  '3',
+        'keterangan' =>  ''
+    ]);
+
+
     }
 }
